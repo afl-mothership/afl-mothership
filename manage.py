c94dd18203a7a2d2ed5844e09bd58ad3029d09d7
Simon Pinfold <simon@uint8.me>
2016-04-28 12:38:29 +1200

    Initial project setup

---
+++ manage.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import os
+
+from flask.ext.script import Manager, Server
+from flask.ext.script.commands import ShowUrls, Clean
+from mothership import create_app
+from mothership.models import db
+# from mothership.models import User
+
+# default to dev config because no one should use this in
+# production anyway
+env = os.environ.get('MOTHERSHIP_ENV', 'dev')
+app = create_app('mothership.settings.%sConfig' % env.capitalize())
+
+manager = Manager(app)
+manager.add_command("server", Server())
+manager.add_command("show-urls", ShowUrls())
+manager.add_command("clean", Clean())
+
+
+@manager.shell
+def make_shell_context():
+	""" Creates a python REPL with several default imports
+		in the context of the app
+	"""
+
+	return dict(app=app, db=db)
+	# return dict(app=app, db=db, User=User)
+
+
+@manager.command
+def createdb():
+	""" Creates a database with all of the tables defined in
+		your SQLAlchemy models
+	"""
+
+	db.create_all()
+
+if __name__ == "__main__":
+	manager.run()
---
+++ mothership/__init__.py
@@ -0,0 +1,59 @@
+#! ../env/bin/python
+# -*- coding: utf-8 -*-
+
+__author__ = 'Simon Pinfold'
+__email__ = 'simon@uint8.me'
+__version__ = '1.0'
+
+from flask import Flask
+from webassets.loaders import PythonLoader as PythonAssetsLoader
+
+from mothership.controllers.main import main
+from mothership import assets
+from mothership.models import db
+
+from mothership.extensions import (
+	cache,
+	assets_env,
+	debug_toolbar,
+	login_manager
+)
+
+
+def create_app(object_name):
+	"""
+	An flask application factory, as explained here:
+	http://flask.pocoo.org/docs/patterns/appfactories/
+
+	Arguments:
+		object_name: the python path of the config object,
+					 e.g. mothership.settings.ProdConfig
+
+		env: The name of the current environment, e.g. prod or dev
+	"""
+
+	app = Flask(__name__)
+
+	app.config.from_object(object_name)
+
+	# initialize the cache
+	cache.init_app(app)
+
+	# initialize the debug tool bar
+	debug_toolbar.init_app(app)
+
+	# initialize SQLAlchemy
+	db.init_app(app)
+
+	login_manager.init_app(app)
+
+	# Import and register the different asset bundles
+	assets_env.init_app(app)
+	assets_loader = PythonAssetsLoader(assets)
+	for name, bundle in assets_loader.load_bundles().items():
+		assets_env.register(name, bundle)
+
+	# register our blueprints
+	app.register_blueprint(main)
+
+	return app
